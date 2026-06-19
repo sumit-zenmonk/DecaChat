fe5877b8a69777ccb0366fc662bee6c63aee418f
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-06-19 11:02:24 +0530

    css improved

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,9 +1,12 @@
 "use client"
 
+import { Box } from "@mui/material";
+import styles from "./auth.module.css";
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        <>
+        <Box className={styles.layout}>
             {children}
-        </>
+        </Box>
     );
 }
